c93835110488fbd144e9d3a06a57a3667dee7f04
Stephanie M. Gogarten <sdmorris@uw.edu>
2021-04-29 13:41:31 -0700

    update script name

--- test/test_XYchrom_depth.sh
+++ test/test_XYchrom_depth.sh
@@ -6,7 +6,7 @@ BASE_PATH=$1
 R -q --vanilla --args \
   --coverage_file $BASE_PATH/test/data/coverage/*_coverage.txt \
   --out_prefix test \
-  < $BASE_PATH/R/coverage.R
+  < $BASE_PATH/R/XYchrom_depth.R
 
 R -q --vanilla --args test_norm_depth.rds \
   < $BASE_PATH/test/check_out_file.R
